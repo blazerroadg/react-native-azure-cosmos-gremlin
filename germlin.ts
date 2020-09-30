655724a9f717075eb48e13bb8adfe12b2cd510fa
blazerroad <blazer.road@gmail.com>
2020-09-30 14:15:03 -0400

    Cache added

--- germlin.ts
+++ germlin.ts
@@ -1,4 +1,5 @@
 import CryptoJS from 'crypto-js'
+import { cache } from 'react-native-fetch-cache/cacheResolver'
 
 interface AzureConfig {
     masterkey: string;
@@ -17,7 +18,8 @@ class Param {
 
 interface FetchParam {
     query: string;
-    parameters: Array<Param>
+    parameters: Array<Param>;
+    stateName: string
 }
 class AuthParam {
     constructor(paramscount: number, methodname: string, date: string, key: string) {
@@ -44,6 +46,14 @@ class AzureGermlin {
     }
 
     public fetch(param: FetchParam): Promise<Response> {
+        const isValid = cache(param.stateName, undefined)
+        if (isValid) {
+            const response = new Response(null, { status: 304, statusText: "cache" })
+            return new Promise<Response>((resolve, reject) => {
+                resolve(response)
+            });
+        }
+
         const today = new Date();
         const UTCstring = today.toUTCString();
         const auth = this.auth(new AuthParam(param.parameters.length, AzureGermlin.config.methodname, UTCstring, AzureGermlin.config.masterkey))
